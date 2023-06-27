cac78a11111c088bcc31168295d0590c5126151a
Pavlov Kostyantyn <realanka77@gmail.com>
2023-06-27 19:16:12 +0300

    feat: solve 02 test

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,19 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 4 },
+  // continue cases for other actions
+];
 
+const testCasesArray = testCases.map((item) => Object.values(item));
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
-  });
-  // Consider to use Jest table tests API to test all cases above
+  test.each(testCasesArray)(
+    'arguments %i and %i with action %s should be equal %i',
+    (a, b, action, expected) => {
+      expect(simpleCalculator({ a, b, action })).toBe(expected);
+    },
+  );
 });
