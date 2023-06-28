a91cfc73d37555a90169ba3c966d6124b18fce48
Olga Chernega <eliang.cheng@gmail.com>
2023-06-28 17:28:02 +0300

    feat: add table-tests module solution

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,36 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+  { a: 5, b: 2, action: Action.Subtract, expected: 3 },
+  { a: 4, b: 2, action: Action.Multiply, expected: 8 },
+  { a: 10, b: 2, action: Action.Divide, expected: 5 },
+  { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
+  { a: 0, b: 5, action: Action.Add, expected: 5 },
+  { a: -5, b: 2, action: Action.Multiply, expected: -10 },
+  { a: 10, b: 0, action: Action.Divide, expected: Infinity },
+  { a: 2, b: 0, action: Action.Exponentiate, expected: 1 },
+  { a: 1, b: 0, action: Action.Divide, expected: Infinity },
+  { a: -3, b: 2, action: Action.Exponentiate, expected: 9 },
+  { a: 2, b: 0.5, action: Action.Exponentiate, expected: 1.4142135623730951 },
+  { a: 1, b: 2, action: '%', expected: null },
+  { a: 2, b: 3, action: '**', expected: null },
+  { a: 3, b: 2, action: '++', expected: null },
+  { a: '5', b: 2, action: Action.Add, expected: null },
+  { a: 2, b: false, action: Action.Add, expected: null },
+  { a: null, b: 2, action: Action.Add, expected: null },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  testCases.forEach((testCase) => {
+    const { a, b, action, expected } = testCase;
+
+    test(`should perform ${action} operation on ${a} and ${b} and return ${expected}`, () => {
+      const rawInput = { a, b, action };
+      const result = simpleCalculator(rawInput);
+      expect(result).toBe(expected);
+    });
   });
-  // Consider to use Jest table tests API to test all cases above
 });
