0d84d761cf26fcaf4e4d5bd42bce904eebace813
vladislavl-hyuna <vladislav.lisniy@reddtech.io>
2023-06-28 17:40:00 +0300

    feat: implement the table-driven testing approach

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,30 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 5, b: 5, action: Action.Add, expected: 10 },
+  { a: -15, b: 5, action: Action.Add, expected: -10 },
+  { a: 15, b: 5, action: Action.Subtract, expected: 10 },
+  { a: 5, b: 15, action: Action.Subtract, expected: -10 },
+  { a: 5, b: 5, action: Action.Multiply, expected: 25 },
+  { a: -5, b: 5, action: Action.Multiply, expected: -25 },
+  { a: 25, b: 5, action: Action.Divide, expected: 5 },
+  { a: -25, b: 5, action: Action.Divide, expected: -5 },
+  { a: 25, b: 5, action: Action.Exponentiate, expected: 9765625 },
+  { a: -25, b: 5, action: Action.Exponentiate, expected: -9765625 },
+  { a: 25, b: 5, action: '|', expected: null },
+  { a: 5, b: 'string', action: Action.Add, expected: null },
+  { a: 5, b: null, action: Action.Subtract, expected: null },
+  { a: 'string', b: 5, action: Action.Divide, expected: null },
+  { a: 'string', b: 'string', action: Action.Multiply, expected: null },
+  { a: null, b: null, action: Action.Exponentiate, expected: null },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
-  });
-  // Consider to use Jest table tests API to test all cases above
+  test.each(testCases)(
+    'expects $a $action $b to be $expected',
+    ({ a, b, action, expected }) => {
+      expect(simpleCalculator({ a, b, action })).toBe(expected);
+    },
+  );
 });
