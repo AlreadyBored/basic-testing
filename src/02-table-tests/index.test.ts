ef3c6472be905f24c1edd68b86465da2470665d9
lgklsv <kolosov20011@gmail.com>
2023-06-28 23:17:30 +0300

    feat: create table test for the calculator

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,31 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+  { a: 3, b: 2, action: Action.Subtract, expected: 1 },
+  { a: 10, b: -4, action: Action.Subtract, expected: 14 },
+  { a: 6, b: 2, action: Action.Subtract, expected: 4 },
+  { a: 6, b: 2, action: Action.Multiply, expected: 12 },
+  { a: 10, b: -10, action: Action.Multiply, expected: -100 },
+  { a: 5, b: 5, action: Action.Multiply, expected: 25 },
+  { a: 100, b: 5, action: Action.Divide, expected: 20 },
+  { a: 50, b: 10, action: Action.Divide, expected: 5 },
+  { a: 63, b: 9, action: Action.Divide, expected: 7 },
+  { a: 2, b: 5, action: Action.Exponentiate, expected: 32 },
+  { a: 3, b: 3, action: Action.Exponentiate, expected: 27 },
+  { a: 4, b: 2, action: Action.Exponentiate, expected: 16 },
+  { a: '32', b: 2, action: Action.Add, expected: null },
+  { a: true, b: 2, action: Action.Add, expected: null },
+  { a: 3, b: null, action: Action.Add, expected: null },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
-  });
-  // Consider to use Jest table tests API to test all cases above
+  test.each(testCases)(
+    'simpleCalculator({ $a, $b, $action }) should be %expected',
+    ({ a, b, action, expected }) => {
+      expect(simpleCalculator({ a, b, action })).toEqual(expected);
+    },
+  );
 });
