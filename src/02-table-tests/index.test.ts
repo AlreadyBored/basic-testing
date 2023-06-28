88afc79ceb8f391fe4fbcd5172f05d04a56563f7
Artsiom Ostrovskiy <ostrovskiy.a.97@gmail.com>
2023-06-28 22:22:26 +0200

    refactor: delete unnecessary comments

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,4 +1,3 @@
-// Uncomment the code below and write your tests
 import { simpleCalculator, Action } from './index';
 
 const testCases = [
@@ -9,12 +8,10 @@ const testCases = [
   { a: 3, b: 2, action: Action.Subtract, expected: 1 },
   { a: 4, b: 2, action: Action.Divide, expected: 2 },
   { a: 5, b: 2, action: Action.Exponentiate, expected: 25 },
-  // continue cases for other actions
 ];
 
 describe.each(testCases)('simpleCalculator', ({ a, b, action, expected }) => {
   test(`Expression ${a} ${action} ${b} should be equal to ${expected}`, () => {
     expect(simpleCalculator({ a, b, action })).toBe(expected);
   });
-  // Consider to use Jest table tests API to test all cases above
 });
