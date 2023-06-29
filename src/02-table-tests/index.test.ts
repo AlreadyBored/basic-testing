bd2d01927d302e2cf2ddc9b5b5f641bde7b33223
Rasmiyya-Mammadli <rashababayeva@gmail.com>
2023-06-30 00:25:32 +0400

    feat: add jest test to task 01 and 02

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,38 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+  { a: 1, b: 2, action: Action.Subtract, expected: -1 },
+  { a: 2, b: 2, action: Action.Subtract, expected: 0 },
+  { a: 3, b: 2, action: Action.Subtract, expected: 1 },
+  { a: 1, b: 2, action: Action.Multiply, expected: 2 },
+  { a: 2, b: 2, action: Action.Multiply, expected: 4 },
+  { a: 3, b: 2, action: Action.Multiply, expected: 6 },
+  { a: 1, b: 2, action: Action.Divide, expected: 0.5 },
+  { a: 2, b: 2, action: Action.Divide, expected: 1 },
+  { a: 3, b: 2, action: Action.Divide, expected: 1.5 },
+  { a: 1, b: 2, action: Action.Exponentiate, expected: 1 },
+  { a: 2, b: 2, action: Action.Exponentiate, expected: 4 },
+  { a: 3, b: 2, action: Action.Exponentiate, expected: 9 },
+  // Add more test cases for other actions and expected results
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  test('should pass table tests', () => {
+    testCases.forEach(({ a, b, action, expected }) => {
+      const result = simpleCalculator({ a, b, action });
+      expect(result).toBe(expected);
+    });
   });
-  // Consider to use Jest table tests API to test all cases above
+
+  test.each(testCases)('%action(%i, %i)', ({ a, b, action, expected }) => {
+    const result = simpleCalculator({
+      a: a,
+      b: b,
+      action: action,
+    });
+    expect(result).toBe(expected);
+  });
 });
