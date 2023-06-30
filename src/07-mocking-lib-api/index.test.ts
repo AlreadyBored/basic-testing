d5b909209f5d4a8e5f207e4df03a4e3aa805b76d
Artsiom Ostrovskiy <ostrovskiy.a.97@gmail.com>
2023-06-30 19:23:36 +0200

    feat: integrate tests for mocking-lib-api module

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,51 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
 
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const axiosCreateMock = jest.spyOn(axios, 'create');
+
+    await throttledGetDataFromApi('/users');
+    jest.runAllTimers();
+
+    const passedBaseUrl = axiosCreateMock.mock.calls[0]?.[0]?.baseURL;
+    const returnedInstance = axiosCreateMock.mock.results[0]?.value;
+    expect(returnedInstance.defaults.baseURL).toBe(passedBaseUrl);
+
+    axiosCreateMock.mockRestore();
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const axiosGetMock = jest.spyOn(axios.Axios.prototype, 'get');
+    const path = '/users';
+
+    await throttledGetDataFromApi(path);
+    jest.runAllTimers();
+
+    const relativePath = axiosGetMock.mock.calls[0]?.[0];
+    expect(relativePath).toBe(path);
+
+    axiosGetMock.mockRestore();
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const returnedData = { data: [{ user: 'Vladimir' }] };
+    const axiosGetMock = jest.spyOn(axios.Axios.prototype, 'get');
+    axiosGetMock.mockResolvedValue(returnedData);
+
+    const result = await throttledGetDataFromApi('users');
+    jest.runAllTimers();
+
+    await expect(result).toBe(returnedData.data);
+
+    axiosGetMock.mockRestore();
   });
 });
