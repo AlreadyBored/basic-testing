ad2e3d3d3de1e68413d731e9eeffdbedf34b2d54
HelenaIsh <ishmatova@skbkontur.ru>
2023-07-01 18:34:04 +0600

    feat: implement snapshot testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,31 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const values = [1];
+    const expectedLinkedList = {
+      value: 1,
+      next: {
+        value: null,
+        next: null,
+      },
+    };
+    const result = generateLinkedList(values);
+    expect(result).toStrictEqual(expectedLinkedList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const values = [2];
+    const expectedLinkedList = {
+      value: 2,
+      next: {
+        value: null,
+        next: null,
+      },
+    };
+    const result = generateLinkedList(values);
+    expect(result).toStrictEqual(expectedLinkedList);
   });
 });
