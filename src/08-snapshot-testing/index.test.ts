3fe5892f3d5d5870747aeb03287d855e5dbac905
Oleg Semenov <semoleg1986@gmail.com>
2023-07-02 01:44:35 +0300

    feat: 08-snapshot-testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,42 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const linkedList = generateLinkedList([1, 2, 3]);
+    expect(linkedList).toStrictEqual({
+      next: {
+        next: {
+          next: {
+            next: null,
+            value: null,
+          },
+          value: 3,
+        },
+        value: 2,
+      },
+      value: 1,
+    });
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const linkedList = generateLinkedList([1, 2, 3]);
+    expect(linkedList).toMatchInlineSnapshot(`
+      {
+        "next": {
+          "next": {
+            "next": {
+              "next": null,
+              "value": null,
+            },
+            "value": 3,
+          },
+          "value": 2,
+        },
+        "value": 1,
+      }
+    `);
   });
 });
