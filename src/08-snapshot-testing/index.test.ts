a0069c3fb12b8b5aaee476908f22b7c03e23f63e
vladislavl-hyuna <vladislav.lisniy@reddtech.io>
2023-07-02 17:58:40 +0300

    feat: implement Snapshot testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,27 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const values = ['val1', 'val2', 'val3'];
+    const linkList = generateLinkedList(values);
+    const expectedLinkList = {
+      next: {
+        next: { next: { next: null, value: null }, value: 'val3' },
+        value: 'val2',
+      },
+      value: 'val1',
+    };
+
+    expect(linkList).toStrictEqual(expectedLinkList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const values = ['val4', 'val5', 'val6'];
+    const linkList = generateLinkedList(values);
+
+    expect(linkList).toMatchSnapshot();
   });
 });
