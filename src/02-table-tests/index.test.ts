941dd6badd9e72e62af8771157656f54caccc353
Ziyodbek Ahmadjonov <mr.prgrmr03@gmail.com>
2023-07-02 19:57:10 +0500

    feat: create first 3 tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,24 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  testCases.forEach(({ a, b, action, expected }) => {
+    test(`result for: ${action}`, () => {
+      const result = simpleCalculator({
+        a,
+        b,
+        action,
+      });
+      if (expected === null) {
+        expect(result).toBeNull();
+      } else {
+        expect(result).toEqual(expected);
+      }
+    });
   });
-  // Consider to use Jest table tests API to test all cases above
 });
