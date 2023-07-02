bb7fa9fedf937f8c971e309bb7922ac25d9ebbb7
jsenjoyer <jsenjoyer@icloud.com>
2023-07-02 22:17:47 +0200

    02-done

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,25 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { Action, simpleCalculator } from './index';
 
-const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+type TestCase = {
+  a: number;
+  b: number;
+  action: Action;
+  expected: number;
+};
+
+const testCases: TestCase[] = [
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 1, b: 2, action: Action.Subtract, expected: -1 },
+  { a: 10, b: 2, action: Action.Divide, expected: 5 },
+  { a: 10, b: 2, action: Action.Multiply, expected: 20 },
+  { a: 2, b: 4, action: Action.Exponentiate, expected: 16 },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
-  });
-  // Consider to use Jest table tests API to test all cases above
+  test.each(testCases)(
+    'Operation %s of %d and %d should be %d',
+    ({ action, a, b, expected }) => {
+      expect(simpleCalculator({ a, b, action })).toBe(expected);
+    },
+  );
 });
