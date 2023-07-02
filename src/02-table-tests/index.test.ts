d9a81440078f486906a4305ef6ed2aa1fc5910df
Pavlov Kostyantyn <realanka77@gmail.com>
2023-07-02 19:54:00 +0300

    feat: add 1 and 2 tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,18 +1,20 @@
-// Uncomment the code below and write your tests
 import { simpleCalculator, Action } from './index';
 
 const testCases = [
   { a: 1, b: 2, action: Action.Add, expected: 3 },
   { a: 2, b: 2, action: Action.Add, expected: 4 },
-  { a: 3, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+  { a: 3, b: 2, action: Action.Subtract, expected: 1 },
+  { a: 3, b: 2, action: Action.Multiply, expected: 6 },
+  { a: 6, b: 2, action: Action.Divide, expected: 3 },
+  { a: 6, b: 2, action: Action.Exponentiate, expected: 36 },
   // continue cases for other actions
 ];
 
-const testCasesArray = testCases.map((item) => Object.values(item));
 describe('simpleCalculator', () => {
-  test.each(testCasesArray)(
-    'arguments %i and %i with action %s should be equal %i',
-    (a, b, action, expected) => {
+  test.each(testCases)(
+    'arguments $a and $b with action ($action) should be equal $expected',
+    ({ a, b, action, expected }) => {
       expect(simpleCalculator({ a, b, action })).toBe(expected);
     },
   );
