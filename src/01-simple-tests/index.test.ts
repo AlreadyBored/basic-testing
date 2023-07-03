35d7574941af2ec90351de4aafeae46a1e2c8d38
Boris Sivko <boris.sivko@rapidfunnel.com>
2023-07-03 15:34:36 +0300

    impl: first test a,b

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,9 +1,11 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 2, b: 3, action: Action.Add })).toBe(5);
+    expect(simpleCalculator({ a: -2, b: 2, action: Action.Add })).toBe(0);
+    expect(simpleCalculator({ a: 1.2, b: 3.1, action: Action.Add })).toBe(4.3);
   });
 
   test('should subtract two numbers', () => {
