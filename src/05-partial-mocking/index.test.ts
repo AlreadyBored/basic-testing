e97fdda74928c537e15888d072fb215a4f528882
Boris Sivko <boris.sivko@rapidfunnel.com>
2023-07-03 21:39:48 +0300

    init: mock tests

--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -1,8 +1,17 @@
 // Uncomment the code below and write your tests
-// import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
+import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
 
 jest.mock('./index', () => {
-  // const originalModule = jest.requireActual<typeof import('./index')>('./index');
+  const originalModule =
+    jest.requireActual<typeof import('./index')>('./index');
+
+  return {
+    __esModule: true, // Use it when dealing with esModules
+    ...originalModule,
+    mockOne: jest.fn(() => 0),
+    mockTwo: jest.fn(() => 0),
+    mockThree: jest.fn(() => 0),
+  };
 });
 
 describe('partial mocking', () => {
@@ -11,10 +20,16 @@ describe('partial mocking', () => {
   });
 
   test('mockOne, mockTwo, mockThree should not log into console', () => {
-    // Write your test here
+    const spy = jest.spyOn(console, 'log');
+    mockOne();
+    mockTwo();
+    mockThree();
+    expect(spy).not.toHaveBeenCalled();
   });
 
   test('unmockedFunction should log into console', () => {
-    // Write your test here
+    const spy = jest.spyOn(console, 'log');
+    unmockedFunction();
+    expect(spy).toHaveReturned();
   });
 });
