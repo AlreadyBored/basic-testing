46c5b6e5149317311a78b4403a76bd244370dcb8
Gleb <podiumfffuuu@gmail.com>
2023-07-03 13:42:22 +0200

    feat: finish subtask 08

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,35 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
+
+const testValues = [1, 2, 3, 4, 5];
+const testResultList = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: {
+          value: 5,
+          next: {
+            value: null,
+            next: null,
+          },
+        },
+      },
+    },
+  },
+};
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList(testValues)).toStrictEqual(testResultList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList(testValues)).toMatchSnapshot();
   });
 });
