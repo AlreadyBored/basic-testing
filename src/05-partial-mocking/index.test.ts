dea380242cbcd917f1b360ceb9ce60e324861cac
aliya <tusupkalievaaliya921@gmail.com>
2023-07-03 11:49:08 +0500

    feat: add 05-partial-mocking test

--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -1,14 +1,10 @@
 import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
-jest.mock('./index', () => {
-  const originalModule =
-    jest.requireActual<typeof import('./index')>('./index');
-  return {
-    ...originalModule,
-    mockOne: jest.fn(),
-    mockTwo: jest.fn(),
-    mockThree: jest.fn(),
-  };
-});
+jest.mock('./index', () => ({
+  ...jest.requireActual<typeof import('./index')>('./index'),
+  mockOne: jest.fn(),
+  mockTwo: jest.fn(),
+  mockThree: jest.fn(),
+}));
 describe('partial mocking', () => {
   afterAll(() => {
     jest.unmock('./index');
