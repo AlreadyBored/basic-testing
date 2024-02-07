a15e68b09faaba601d2cb2dce6ecabfc2ecc72e8
Viktoriia Fedotkina <viktoriia_fedotkina@epam.com>
2024-02-07 18:43:28 +0200

    add tests 1-3

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,46 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
     // Write your test here
+    const res = simpleCalculator({ a: 1, b: 2, action: Action.Add });
+    expect(res).toBe(3);
   });
 
   test('should subtract two numbers', () => {
     // Write your test here
+    const res = simpleCalculator({ a: 5, b: 3, action: Action.Subtract });
+    expect(res).toBe(2);
   });
 
   test('should multiply two numbers', () => {
     // Write your test here
+    const res = simpleCalculator({ a: 5, b: 2, action: Action.Multiply });
+    expect(res).toBe(10);
   });
 
   test('should divide two numbers', () => {
     // Write your test here
+    const res = simpleCalculator({ a: 15, b: 3, action: Action.Divide });
+    expect(res).toBe(5);
   });
 
   test('should exponentiate two numbers', () => {
     // Write your test here
+    const res = simpleCalculator({ a: 2, b: 3, action: Action.Exponentiate });
+    expect(res).toBe(8);
   });
 
   test('should return null for invalid action', () => {
     // Write your test here
+    const res = simpleCalculator({ a: 2, b: 3, action: '%' });
+    expect(res).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
     // Write your test here
+    const res = simpleCalculator({ a: 'n', b: 3, action: Action.Subtract });
+    expect(res).toBeNull();
   });
 });
--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -1,30 +1,46 @@
 // Uncomment the code below and write your tests
-// import { throwError, throwCustomError, resolveValue, MyAwesomeError, rejectCustomError } from './index';
+import {
+  throwError,
+  throwCustomError,
+  resolveValue,
+  MyAwesomeError,
+  rejectCustomError,
+} from './index';
 
 describe('resolveValue', () => {
   test('should resolve provided value', async () => {
     // Write your test here
+    const res = await resolveValue('test');
+    expect(res).toBe('test');
   });
 });
 
 describe('throwError', () => {
   test('should throw error with provided message', () => {
     // Write your test here
+    expect(() => throwError('test error')).toThrowError('test error');
   });
 
   test('should throw error with default message if message is not provided', () => {
     // Write your test here
+    expect(() => throwError()).toThrowError('Oops!');
   });
 });
 
 describe('throwCustomError', () => {
   test('should throw custom error', () => {
     // Write your test here
+    expect(() => throwCustomError()).toThrowError(MyAwesomeError);
   });
 });
 
 describe('rejectCustomError', () => {
   test('should reject custom error', async () => {
     // Write your test here
+    try {
+      await rejectCustomError();
+    } catch (e) {
+      expect(e).toBeInstanceOf(MyAwesomeError);
+    }
   });
 });
