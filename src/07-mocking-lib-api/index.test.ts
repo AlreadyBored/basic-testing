6b70c0d185175d97bd674ae8506a6912ecb0bec7
Anton D <adkv.lab@gmail.com>
2024-02-07 15:56:30 +0100

    feat: implement test 07

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,35 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios, { Axios } from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+const url = '/posts/1';
 
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const axiosCreateSpy = jest.spyOn(axios, 'create');
+    throttledGetDataFromApi(url);
+    jest.runAllTimers();
+    expect(axiosCreateSpy).toBeCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const axiosGetSpy = jest.spyOn(Axios.prototype, 'get');
+    throttledGetDataFromApi(url);
+    jest.runAllTimers();
+    expect(axiosGetSpy).toBeCalledWith(url);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    expect(throttledGetDataFromApi(url)).resolves.toMatchObject({ id: 1 });
   });
 });
