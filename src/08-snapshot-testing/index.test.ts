1b279df9ed092336af3d9d0017003c22708c5ca2
Anton D <adkv.lab@gmail.com>
2024-02-07 16:30:51 +0100

    feat: implement test 08

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,28 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
+
+const values = [1, 2, false];
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList(values)).toStrictEqual({
+      value: 1,
+      next: {
+        value: 2,
+        next: {
+          value: false,
+          next: {
+            value: null,
+            next: null,
+          },
+        },
+      },
+    });
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList(values)).toMatchSnapshot();
   });
 });
