27b035329f6988a7a95dfae2aa16ccd13db6f2a9
mike-kotikov <37017407+mike-kotikov@users.noreply.github.com>
2024-02-07 23:00:31 +0300

    feat: add unit simple test

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,74 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        a: 478539,
+        b: 758375,
+        action: Action.Add,
+      }),
+    ).toEqual(1236914);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        a: 936813,
+        b: 273485,
+        action: Action.Subtract,
+      }),
+    ).toEqual(663328);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        a: 4723,
+        b: 6791,
+        action: Action.Multiply,
+      }),
+    ).toEqual(32073893);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        a: 9879653,
+        b: 24586,
+        action: Action.Divide,
+      }),
+    ).toEqual(401.8406003416579);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        a: 34,
+        b: 5,
+        action: Action.Exponentiate,
+      }),
+    ).toEqual(45435424);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        a: 8,
+        b: 4,
+        action: 'INVALID ACTION',
+      }),
+    ).toEqual(null);
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    expect(
+      simpleCalculator({
+        a: { prop: 'value' },
+        b: [1, 2, 3],
+        action: Action.Subtract,
+      }),
+    ).toEqual(null);
   });
 });
