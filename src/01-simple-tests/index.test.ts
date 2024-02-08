8c674baa9535bdc0e74e6844cf4ae9ee8f73276b
Olena Datso <elena.shcheglova@gmail.com>
2024-02-07 17:58:14 -0700

    fix: eol

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,32 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    simpleCalculator({ a: 2, b: 3, action: Action.Add });
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    simpleCalculator({ a: 6, b: 3, action: Action.Subtract });
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    simpleCalculator({ a: 2, b: 3, action: Action.Multiply });
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    simpleCalculator({ a: 6, b: 3, action: Action.Divide });
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    simpleCalculator({ a: 2, b: 3, action: Action.Exponentiate });
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    simpleCalculator({ a: 2, b: 3, action: Action });
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    simpleCalculator({ a: '2', b: '3', action: Action.Add });
   });
 });
