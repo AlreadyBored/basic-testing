f681f36388fa92507c426255c790e44cf24fab7d
yolo <igoncharov@flightpath3d.com>
2024-02-08 20:21:41 +0000

    test: implement snapshot-testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,28 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
+  const elements = (length: number) => Array.from({ length }, (_, i) => i);
+
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList(elements(3))).toStrictEqual({
+      value: 0,
+      next: {
+        value: 1,
+        next: {
+          value: 2,
+          next: {
+            value: null,
+            next: null,
+          },
+        },
+      },
+    });
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList(elements(50))).toMatchSnapshot();
   });
 });
