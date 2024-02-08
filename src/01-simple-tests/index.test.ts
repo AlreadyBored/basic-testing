bb9382d763499ba20eff0c099e376d7690c4b0cf
Uladimir Hrydzin <hrydzin@gmail.com>
2024-02-08 19:56:56 +0400

    feat: add implementation tests for simple-tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,41 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 3, b: 4, action: Action.Add })).toBe(7);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 4, b: 3, action: Action.Subtract })).toBe(1);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 4, b: 3, action: Action.Multiply })).toBe(12);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 9, b: 3, action: Action.Divide })).toBe(3);
   });
 
   test('should exponentiate two numbers', () => {
     // Write your test here
+    expect(simpleCalculator({ a: 4, b: 3, action: Action.Exponentiate })).toBe(
+      64,
+    );
   });
 
   test('should return null for invalid action', () => {
     // Write your test here
+    expect(
+      simpleCalculator({ a: 3, b: 4, action: 'invalidAction' }),
+    ).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
     // Write your test here
+    expect(
+      simpleCalculator({ a: 'string', b: {}, action: Action.Add }),
+    ).toBeNull();
   });
 });
