89240c657be8c39959a3dfa0779ddc0e926d7af0
andy_t <andrei.tsylko@rk-software.net>
2024-02-09 13:04:34 +0100

    feat: implement table tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,80 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
-const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+const positiveTestCases = [
+  { a: 1, b: -2, action: Action.Add, expected: -1 },
+  { a: 2, b: 2, action: Action.Subtract, expected: 0 },
+  { a: 3, b: 2, action: Action.Multiply, expected: 6 },
+  { a: 6, b: 2, action: Action.Divide, expected: 3 },
+  { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
+  { a: 2, b: 3, action: 'invalid', expected: null },
+  { a: 'invalid', b: 3, action: Action.Add, expected: null },
+];
+
+const edgeTestCases = [
+  {
+    a: Number.MAX_VALUE,
+    b: Number.MAX_VALUE,
+    action: Action.Add,
+    expected: Infinity,
+  },
+  { a: -0, b: 0, action: Action.Subtract, expected: -0 },
+  {
+    a: Number.MAX_VALUE,
+    b: Number.MAX_VALUE,
+    action: Action.Multiply,
+    expected: Infinity,
+  },
+  { a: 6, b: 0, action: Action.Divide, expected: Infinity },
+  {
+    a: Number.MAX_VALUE,
+    b: Number.MAX_VALUE,
+    action: Action.Exponentiate,
+    expected: Infinity,
+  },
+];
+
+const negativeTestCases = [
+  {
+    a: NaN,
+    b: NaN,
+    action: Action.Add,
+    expected: NaN,
+  },
+
+  {
+    a: Infinity,
+    b: Infinity,
+    action: Action.Multiply,
+    expected: Infinity,
+  },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  describe('should pass positive cases', () => {
+    it.each(positiveTestCases)(
+      'should return $expected when $a is $action $b',
+      ({ a, b, action, expected }) => {
+        expect(simpleCalculator({ a, b, action })).toBe(expected);
+      },
+    );
   });
-  // Consider to use Jest table tests API to test all cases above
+
+  describe('should pass edge cases', () => {
+    it.each(edgeTestCases)(
+      'should return $expected when $a is $action $b',
+      ({ a, b, action, expected }) => {
+        expect(simpleCalculator({ a, b, action })).toBe(expected);
+      },
+    );
+  });
+
+  describe('should pass negative cases', () => {
+    it.each(negativeTestCases)(
+      'should return $expected when $a is $action $b',
+      ({ a, b, action, expected }) => {
+        expect(simpleCalculator({ a, b, action })).toBe(expected);
+      },
+    );
+  });
 });
