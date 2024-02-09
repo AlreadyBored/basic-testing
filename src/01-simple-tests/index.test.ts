02e5b99c71eda34fcb1aaf1eaf99767a09c6f6a5
anton-glebov <anton.glebov@useinsider.com>
2024-02-09 21:13:34 +0300

    feat: add simple tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,88 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
+
+type Test = {
+  [key: string]: {
+    a: number | string;
+    b: number | string;
+    action: Action | null;
+    expected: number | null;
+  };
+};
+
+const tests: Test = {
+  shouldAddTwoNumbers: { a: 2, b: 1, action: Action.Add, expected: 3 },
+  shouldSubtractTwoNumbers: {
+    a: 2,
+    b: 1,
+    action: Action.Subtract,
+    expected: 1,
+  },
+  shouldMultiplyTwoNumbers: {
+    a: 2,
+    b: 1,
+    action: Action.Multiply,
+    expected: 2,
+  },
+  shouldDivideTwoNumbers: {
+    a: 2,
+    b: 1,
+    action: Action.Divide,
+    expected: 2,
+  },
+  shouldExponentiateTwoNumbers: {
+    a: 2,
+    b: 1,
+    action: Action.Exponentiate,
+    expected: 2,
+  },
+  shouldReturnNullForInvalidAction: {
+    a: '2',
+    b: '1',
+    action: Action.Exponentiate,
+    expected: null,
+  },
+  shouldReturnNullForInvalidArguments: {
+    a: 2,
+    b: 1,
+    action: null,
+    expected: null,
+  },
+};
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const input = tests.shouldAddTwoNumbers;
+    input && expect(simpleCalculator(input)).toBe(input?.expected);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const input = tests.shouldSubtractTwoNumbers;
+    input && expect(simpleCalculator(input)).toBe(input?.expected);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const input = tests.shouldMultiplyTwoNumbers;
+    input && expect(simpleCalculator(input)).toBe(input?.expected);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const input = tests.shouldDivideTwoNumbers;
+    input && expect(simpleCalculator(input)).toBe(input?.expected);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const input = tests.shouldExponentiateTwoNumbers;
+    input && expect(simpleCalculator(input)).toBe(input?.expected);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const input = tests.shouldReturnNullForInvalidAction;
+    input && expect(simpleCalculator(input)).toBe(input?.expected);
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const input = tests.shouldReturnNullForInvalidArguments;
+    input && expect(simpleCalculator(input)).toBe(input?.expected);
   });
 });
