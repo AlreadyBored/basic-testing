8eaef864f4d54b54e001b97609abd5163b24ecbc
andy_t <andrei.tsylko@rk-software.net>
2024-02-09 14:16:38 +0100

    feat: implement error handling unit tests

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -1,30 +1,45 @@
 // Uncomment the code below and write your tests
-// import { throwError, throwCustomError, resolveValue, MyAwesomeError, rejectCustomError } from './index';
+import {
+  throwError,
+  throwCustomError,
+  resolveValue,
+  rejectCustomError,
+} from './index';
 
 describe('resolveValue', () => {
   test('should resolve provided value', async () => {
-    // Write your test here
+    const spy = jest.fn();
+    await resolveValue('test').then((value) => spy(value));
+    expect(spy).toHaveBeenCalledWith('test');
   });
 });
 
 describe('throwError', () => {
   test('should throw error with provided message', () => {
-    // Write your test here
+    expect(() => throwError('test')).toThrowError('test');
   });
 
   test('should throw error with default message if message is not provided', () => {
-    // Write your test here
+    expect(() => throwError()).toThrowError('Oops!');
   });
 });
 
 describe('throwCustomError', () => {
   test('should throw custom error', () => {
-    // Write your test here
+    expect(() => throwCustomError).toThrowError(
+      'This is my awesome custom error!',
+    );
   });
 });
 
 describe('rejectCustomError', () => {
   test('should reject custom error', async () => {
-    // Write your test here
+    try {
+      console.log('try');
+      await rejectCustomError();
+    } catch (error) {
+      console.log('error', error);
+      expect(typeof error).toMatch('This is my awesome custom error!');
+    }
   });
 });
