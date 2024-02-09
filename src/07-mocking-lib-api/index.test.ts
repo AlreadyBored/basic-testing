466c0bebca0c472c2682a9cf33bd60d6fe618ccd
dzehil02 <dzehil02@gmail.com>
2024-02-09 22:36:08 +0100

    Add test which create instance with base url

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,62 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios, { Axios } from 'axios';
+import { throttledGetDataFromApi } from './index';
 
 describe('throttledGetDataFromApi', () => {
+  let response: {
+    data: {
+      id: number;
+      name: string;
+      username: string;
+      email: string;
+      phone: string;
+      website: string;
+    }[];
+  };
+
+  beforeEach(() => {
+    response = {
+      data: [
+        {
+          id: 1,
+          name: 'Leanne Graham',
+          username: 'Bret',
+          email: 'Sincere@april.biz',
+          phone: '1-770-736-8031 x56442',
+          website: 'hildegard.org',
+        },
+        {
+          id: 2,
+          name: 'Ervin Howell',
+          username: 'Antonette',
+          email: 'Shanna@melissa.tv',
+          phone: '010-692-6593 x09125',
+          website: 'anastasia.net',
+        },
+      ],
+    };
+    jest.spyOn(Axios.prototype, 'get').mockResolvedValue(response);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const axiosClient = jest.spyOn(axios, 'create');
+    await throttledGetDataFromApi('/test');
+    expect(axiosClient).toBeCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
+    axiosClient.mockRestore();
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    // await throttledGetDataFromApi('/test');
+    // expect(Axios.prototype.get).toBeCalledWith('/test');
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    // const result = await throttledGetDataFromApi('/users');
+    // expect(result).toEqual(response.data);
   });
 });
