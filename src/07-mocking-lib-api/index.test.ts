d4b59c059178278ab7a2ca36f3a7a1865c366a36
yolo <igoncharov@flightpath3d.com>
2024-02-09 14:23:54 +0000

    test: no real usage of axios

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -6,15 +6,19 @@ jest.mock('lodash', () => ({
 }));
 
 describe('throttledGetDataFromApi', () => {
+  let axiosGetSpy: jest.SpyInstance;
   const baseURL = 'https://jsonplaceholder.typicode.com';
-  const endPoint = 'albums/1';
+  const endPoint = 'some endpoint';
+  const response = { data: 'some response' };
+
+  beforeEach(() => {
+    axiosGetSpy = jest
+      .spyOn(axios.Axios.prototype, 'get')
+      .mockResolvedValueOnce({ data: response });
+  });
 
   test('should create instance with provided base url', async () => {
     const createSpy = jest.spyOn(axios, 'create');
-    jest
-      .spyOn(axios.Axios.prototype, 'get')
-      .mockResolvedValueOnce({ data: 'data' }); // mock axios.get because we don't need it in this test
-
     await throttledGetDataFromApi(endPoint);
 
     expect(createSpy).toBeCalledTimes(1);
@@ -22,17 +26,13 @@ describe('throttledGetDataFromApi', () => {
   });
 
   test('should perform request to correct provided url', async () => {
-    const getSpy = jest
-      .spyOn(axios.Axios.prototype, 'get')
-      .mockResolvedValueOnce({ data: 'data' }); // mock axios.get because we don't need it in this test
-
     await throttledGetDataFromApi(endPoint);
 
-    expect(getSpy).toBeCalledTimes(1);
-    expect(getSpy).toBeCalledWith(endPoint);
+    expect(axiosGetSpy).toBeCalledTimes(1);
+    expect(axiosGetSpy).toBeCalledWith(endPoint);
   });
 
   test('should return response data', async () => {
-    expect(await throttledGetDataFromApi(endPoint)).toBeDefined(); // get real api response
+    expect(await throttledGetDataFromApi(endPoint)).toStrictEqual(response);
   });
 });
