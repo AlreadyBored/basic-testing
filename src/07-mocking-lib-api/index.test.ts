46a95927287cfd691f8bf720fc69a903ef4ba67c
Andrew <gandrejko2002@gmail.com>
2024-02-10 19:18:43 +0200

    + mocking lib api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,48 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('throttledGetDataFromApi', () => {
+  const relativePath = '/posts/1';
+  const data = {
+    data: {
+      userId: 1,
+      id: 1,
+      title:
+        'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+      body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+    },
+  };
+  beforeEach(() => {
+    mockedAxios.create.mockReturnValue(mockedAxios);
+    mockedAxios.get.mockResolvedValue(data);
+  });
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const expectedBaseUrl = 'https://jsonplaceholder.typicode.com';
+
+    await throttledGetDataFromApi(relativePath);
+
+    expect(axios.create).toHaveBeenCalledWith({
+      baseURL: expectedBaseUrl,
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const expectedUrl = `https://jsonplaceholder.typicode.com${relativePath}`;
+    const consoleSpy = jest.spyOn(axios, 'get');
+
+    await throttledGetDataFromApi(expectedUrl);
+
+    expect(consoleSpy).toHaveBeenCalledWith(expectedUrl);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const expectedUrl = `https://jsonplaceholder.typicode.com${relativePath}`;
+
+    const result = await throttledGetDataFromApi(expectedUrl);
+
+    expect(result).toEqual(data.data);
   });
 });
