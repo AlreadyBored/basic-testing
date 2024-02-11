15685e75382b9f90a1c9ebf41a5456043c81ceeb
Alla Manshina <moralobsession@gmail.com>
2024-02-11 13:42:40 +0400

    feat: add snapshot tests

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,17 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList([1])).toStrictEqual({
+      next: { next: null, value: null },
+      value: 1,
+    });
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const tree = generateLinkedList(['one', 'two']);
+    expect(tree).toMatchSnapshot();
   });
 });
