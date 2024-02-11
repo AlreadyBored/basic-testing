ff36d8e93be9c6d9dd4b6c79d4b9d0f1aed37a39
andy_t <andrei.tsylko@rk-software.net>
2024-02-11 12:17:40 +0100

    feat: implement snapshot testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,21 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
+//Your task is to use snapshot testing with Jest and compare it to regular comparison testing.
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const elements = [1, 2, 3, 4, 5];
+
+    const linkedList = generateLinkedList(elements);
+
+    expect(linkedList).toMatchSnapshot();
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const elements = [null, 0.1, 2, 13];
+    const linkedList = generateLinkedList(elements);
+    expect(linkedList).toMatchSnapshot();
   });
 });
