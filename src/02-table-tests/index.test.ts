8927cb82e1a3639915742662e7632e50941e8f37
krak <alexedtrifonov@gmail.com>
2024-02-12 21:06:17 +0100

    feat: implement 02-table-tests and 03-error-handling-async solution

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,26 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import {  simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+    
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+  // Add more test cases for other actions
+  { a: 5, b: 3, action: Action.Subtract, expected: 2 },
+  { a: 6, b: 3, action: Action.Divide, expected: 2 },
+  { a: 5, b: 3, action: Action.Multiply, expected: 15 },
+  { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
+  // Add invalid test cases
+  { a: 'invalid', b: 3, action: Action.Add, expected: null },
+  { a: 2, b: 'invalid', action: Action.Add, expected: null },
+  { a: 2, b: 3, action: 'INVALID' as Action, expected: null },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  testCases.forEach(({ a, b, action, expected }) => {
+    test(`should perform ${action} operation correctly`, () => {
+      expect(simpleCalculator({ a, b, action })).toEqual(expected);
+    });
   });
-  // Consider to use Jest table tests API to test all cases above
 });
