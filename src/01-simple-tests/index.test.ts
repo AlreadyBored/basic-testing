97d5cde7707568af9304d351de6359e8d88882fe
freen <freennnn@gmail.com>
2024-02-12 09:37:01 +0100

    feat: add part-1 expect().toBe()

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,36 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+ import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 1, b: 1, action: Action.Add })).toBe(2);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator({ a: 4, b: 2, action: Action.Subtract })).toBe(2);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator( { a: 2, b: 2, action: Action.Multiply })).toBe(4);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator( { a: 9, b: 3, action: Action.Divide })).toBe(3);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    expect(simpleCalculator( { a: 3, b: 3, action: Action.Exponentiate })).toBe(27);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    expect(simpleCalculator( { a: 3, b: 3, action: 'Do I look like an action?' })).toBe(null);
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    expect(simpleCalculator( { a: 'A', b: 'B', action: 'Sideli na trube' })).toBeNull(); // are error mesages with toBeNull() really nicer? 
+  });
+  
+  test('should return null for invalid arguments', () => {
+    expect(simpleCalculator( { a: 2, b: 2, action: Action.Add })).toBeNull(); // are error mesages with toBeNull() really nicer? 
   });
 });
