53b4d362c698dc20d510f3a1fb37542ef1dd8958
mike-kotikov <37017407+mike-kotikov@users.noreply.github.com>
2024-02-13 00:36:00 +0300

    refactor: change jest config to not restore mocks after each test

--- jest.config.js
+++ jest.config.js
@@ -6,8 +6,6 @@ module.exports = {
   moduleNameMapper: {
     '^axios$': require.resolve('axios'),
   },
-  restoreMocks: true,
-  resetMocks: true,
   moduleDirectories: ['node_modules', '<rootDir>/src'],
   transform: {
     '^.+\\.ts?$': 'ts-jest',
