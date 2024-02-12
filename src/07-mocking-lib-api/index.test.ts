195b0128aa2794cfe4f0c7a8111c02cb62f90d9d
mike-kotikov <37017407+mike-kotikov@users.noreply.github.com>
2024-02-13 00:37:04 +0300

    feat: add tests for axios fetch function

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,35 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
 
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => {
+    axios.create = jest.fn(() => axios);
+    // @ts-expect-error ts doesn't see that axios is mocked
+    axios.get.mockImplementation(() => {
+      return Promise.resolve({
+        data: [{ id: 1, name: 'books' }],
+      });
+    });
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    await throttledGetDataFromApi('');
+
+    return expect(axios.create).toHaveBeenLastCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    // TODO: implement test
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const result = await throttledGetDataFromApi('/some/url');
+
+    return expect(result).toStrictEqual([{ id: 1, name: 'books' }]);
   });
 });
