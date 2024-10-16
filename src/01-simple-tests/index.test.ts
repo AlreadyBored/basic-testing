c5fc26e16dbfb72a73ad8e5478f6b82c073fca34
surface74 <surface.msn@gmail.com>
2024-10-16 23:38:59 +0300

    feat: implement tests 01

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,55 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const data = { a: 12, b: 13, action: Action.Add };
+    const expected = data.a + data.b;
+
+    expect(simpleCalculator(data)).toBe(expected);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const data = { a: 12, b: 13, action: Action.Subtract };
+    const expected = data.a - data.b;
+
+    expect(simpleCalculator(data)).toBe(expected);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const data = { a: 12, b: 13, action: Action.Multiply };
+    const expected = data.a * data.b;
+
+    expect(simpleCalculator(data)).toBe(expected);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const data = { a: 12, b: 13, action: Action.Divide };
+    const expected = data.a / data.b;
+
+    expect(simpleCalculator(data)).toBe(expected);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const data = { a: 12, b: 13, action: Action.Exponentiate };
+    const expected = data.a ** data.b;
+
+    expect(simpleCalculator(data)).toBe(expected);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const data = { a: 12, b: 13, action: 'SQRT' };
+
+    expect(simpleCalculator(data)).toBe(null);
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const data1 = { a: '12', b: 13, action: Action.Add };
+    const data2 = { a: 12, b: null, action: Action.Add };
+    const data3 = { a: undefined, b: undefined, action: Action.Add };
+
+    expect(simpleCalculator(data1)).toBe(null);
+    expect(simpleCalculator(data2)).toBe(null);
+    expect(simpleCalculator(data3)).toBe(null);
   });
 });
