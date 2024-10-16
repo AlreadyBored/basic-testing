d2901b82dd4344eaff4634d17f8c4ef4adcf9452
rakr0819 <raman.krauchanka@netcracker.com>
2024-10-17 01:19:45 +0300

    feat: add snapshot testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,26 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
+  const snapshotList = generateLinkedList([
+    {
+      value: 1,
+      next: null,
+    },
+  ]);
+  const otherList = generateLinkedList([
+    {
+      value: 2,
+      next: null,
+    },
+  ]);
+
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(snapshotList).not.toStrictEqual(otherList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(snapshotList).toMatchSnapshot();
   });
 });
