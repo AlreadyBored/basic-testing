e2810d4a7269534c02612cf3405ae90d7909e5a7
surface74 <surface.msn@gmail.com>
2024-10-17 10:06:07 +0300

    feat: implement tests 07

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,40 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios', () => {
+  return {
+    create: () => {
+      return {
+        get: jest.fn().mockImplementation((path: string) => ({ data: path })),
+      };
+    },
+  };
+});
+
+jest.mock('lodash', () => {
+  return {
+    throttle: jest.fn().mockImplementation((fn) => fn),
+  };
+});
 
 describe('throttledGetDataFromApi', () => {
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const create = jest.spyOn(axios, 'create');
+
+    await throttledGetDataFromApi('todos');
+    expect(create).toBeCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const requestedUrl = 'todos';
+    const data = await throttledGetDataFromApi(requestedUrl);
+    expect(data).toBe(requestedUrl);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const data = await throttledGetDataFromApi('todos');
+    expect(data).toBeTruthy();
   });
 });
