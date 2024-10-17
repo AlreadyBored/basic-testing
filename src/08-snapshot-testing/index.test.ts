f9ebb243c853e0443efc0803e864af8db79fb949
surface74 <surface.msn@gmail.com>
2024-10-17 10:14:13 +0300

    feat: implement tests 08

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,25 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
+
+const values1 = [{ value: 1, next: null }];
+const values2 = [
+  { value: 1, next: null },
+  { value: 2, next: null },
+];
+
+const expected = {
+  value: { value: 1, next: null },
+  next: { value: null, next: null },
+};
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList(values1)).toStrictEqual(expected);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const expected = generateLinkedList(values2);
+    expect(expected).toMatchSnapshot();
   });
 });
