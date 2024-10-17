41b493907b27a9748d003ccc9adca3b7e8e355c5
Victor Mitskevich <vmitskevich@solvd.com>
2024-10-17 17:33:05 +0200

    feat: implement partial mocking

--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -1,8 +1,17 @@
 // Uncomment the code below and write your tests
-// import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
+import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
 
 jest.mock('./index', () => {
-  // const originalModule = jest.requireActual<typeof import('./index')>('./index');
+  const originalModule =
+    jest.requireActual<typeof import('./index')>('./index');
+
+  return {
+    __esModule: true,
+    ...originalModule,
+    mockOne: () => null,
+    mockTwo: () => null,
+    mockThree: () => null,
+  };
 });
 
 describe('partial mocking', () => {
@@ -11,10 +20,16 @@ describe('partial mocking', () => {
   });
 
   test('mockOne, mockTwo, mockThree should not log into console', () => {
-    // Write your test here
+    expect(mockOne()).toBeNull();
+    expect(mockTwo()).toBeNull();
+    expect(mockThree()).toBeNull();
   });
 
   test('unmockedFunction should log into console', () => {
-    // Write your test here
+    const logSpy = jest.spyOn(global.console, 'log');
+
+    unmockedFunction();
+
+    expect(logSpy).toHaveBeenCalledWith('I am not mocked');
   });
 });
