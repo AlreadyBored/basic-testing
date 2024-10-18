269e5bff499a80f004b9b76b74f2fe42d73c2cd6
katyastan <134015338+katyastan@users.noreply.github.com>
2024-10-19 00:55:37 +0300

    feat: add tests for mocking-lib-api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,45 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
+jest.mock('lodash', () => ({
+  ...jest.requireActual('lodash'),
+  throttle: jest.fn((fn) => fn), 
+}));
 
 describe('throttledGetDataFromApi', () => {
-  test('should create instance with provided base url', async () => {
-    // Write your test here
+  let mockAxiosClient: any;
+
+  beforeEach(() => {
+    mockAxiosClient = { get: jest.fn() };
+    (axios.create as jest.Mock).mockReturnValue(mockAxiosClient);
   });
 
+  afterEach(() => {
+    jest.clearAllMocks(); 
+  });
+
+  test('should create instance with provided base url', async () => {
+    const relativePath = '/posts/1';
+    const mockResponseData = { id: 1, title: 'Post Title' };
+    mockAxiosClient.get.mockResolvedValueOnce({ data: mockResponseData });
+    await throttledGetDataFromApi(relativePath);
+    expect(axios.create).toHaveBeenCalledWith({ baseURL: 'https://jsonplaceholder.typicode.com' });
+    });
+
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const relativePath = '/posts/1';
+    const mockResponseData = { id: 1, title: 'Post Title' };
+    mockAxiosClient.get.mockResolvedValueOnce({ data: mockResponseData });
+    await throttledGetDataFromApi(relativePath);
+    expect(mockAxiosClient.get).toHaveBeenCalledWith(relativePath);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const relativePath = '/posts/1';
+    const mockResponseData = { id: 1, title: 'Post Title' };
+    mockAxiosClient.get.mockResolvedValueOnce({ data: mockResponseData });
+    const result = await throttledGetDataFromApi(relativePath);
+    expect(result).toEqual(mockResponseData);
   });
 });
