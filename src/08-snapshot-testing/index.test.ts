0fe4df2942f8a1d9df87eadcb02cda1f78b79a13
Victor Mitskevich <vmitskevich@solvd.com>
2024-10-18 17:15:00 +0200

    feat: implement snapshot testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,29 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
+
+const mockedLinkedList = {
+  next: {
+    next: {
+      next: {
+        next: null,
+        value: null,
+      },
+      value: 1,
+    },
+    value: 1,
+  },
+  value: 1,
+};
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList([1, 1, 1])).toStrictEqual(mockedLinkedList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
     // Write your test here
+    expect(generateLinkedList([2, 2, 2])).toMatchSnapshot();
   });
 });
