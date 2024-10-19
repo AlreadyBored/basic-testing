4c89e08eac262d1ef2d7f04278b6dd939727dd85
Igor Bartashevich <86968311+bartigorfs@users.noreply.github.com>
2024-10-20 01:41:54 +0300

    feat: mocking lib api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,53 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+type AxiosResponse = {
+  data: string;
+};
+
+type ApiTestSettings = {
+  URL: string;
+  Endpoint: string;
+  Response: AxiosResponse;
+};
+
+jest.mock('lodash', () => ({
+  throttle: jest.fn((fn) => fn),
+}));
 
 describe('throttledGetDataFromApi', () => {
-  test('should create instance with provided base url', async () => {
-    // Write your test here
+  const settings: ApiTestSettings = {
+    URL: 'https://jsonplaceholder.typicode.com',
+    Endpoint: '/todos/1',
+    Response: { data: 'response' },
+  };
+  let AxiosGetSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    AxiosGetSpy = jest
+      .spyOn(axios.Axios.prototype, 'get')
+      .mockResolvedValue(settings.Response);
   });
 
-  test('should perform request to correct provided url', async () => {
-    // Write your test here
+  test('should create an instance with provided base url', async () => {
+    const axiosCreateSpy = jest.spyOn(axios, 'create');
+    await throttledGetDataFromApi(settings.Endpoint);
+
+    expect(axiosCreateSpy).toHaveBeenCalledTimes(1);
+    expect(axiosCreateSpy).toHaveBeenCalledWith({ baseURL: settings.URL });
+  });
+
+  test('should perform a request to the correct provided url', async () => {
+    await throttledGetDataFromApi(settings.URL);
+
+    expect(AxiosGetSpy).toHaveBeenCalledTimes(1);
+    expect(AxiosGetSpy).toHaveBeenCalledWith(settings.URL);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const responseData = await throttledGetDataFromApi(settings.Endpoint);
+
+    expect(responseData).toEqual(settings.Response.data);
   });
 });
