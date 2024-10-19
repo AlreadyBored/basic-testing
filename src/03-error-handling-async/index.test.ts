ae46fd9833378ae37d54e5765effc7a3e303ace1
giorgi khoshtaria <99177635+g1org11@users.noreply.github.com>
2024-10-19 16:29:50 +0400

    add error-tests

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -1,30 +1,38 @@
+/* eslint-disable prettier/prettier */
 // Uncomment the code below and write your tests
-// import { throwError, throwCustomError, resolveValue, MyAwesomeError, rejectCustomError } from './index';
-
-describe('resolveValue', () => {
-  test('should resolve provided value', async () => {
-    // Write your test here
+import {
+  throwError,
+  throwCustomError,
+  resolveValue,
+  MyAwesomeError,
+  rejectCustomError,
+} from './index';
+describe('resolveValue', (): void => {
+  test('should resolve provided value', async (): Promise<void> => {
+    const value = 'Resolved value';
+    await expect(resolveValue(value)).resolves.toBe(value);
   });
 });
 
-describe('throwError', () => {
-  test('should throw error with provided message', () => {
-    // Write your test here
+describe('throwError', (): void => {
+  test('should throw error with provided message', (): void => {
+    const msg = 'Operation failed';
+    expect(() => throwError(msg)).toThrowError(new Error(msg));
   });
 
-  test('should throw error with default message if message is not provided', () => {
-    // Write your test here
-  });
-});
-
-describe('throwCustomError', () => {
-  test('should throw custom error', () => {
-    // Write your test here
+  test('should throw error with default message if message is not provided', (): void => {
+    const defaultMessage = 'message is not provided';
+    expect(() => throwError()).toThrowError(new Error(defaultMessage));
   });
 });
 
-describe('rejectCustomError', () => {
-  test('should reject custom error', async () => {
-    // Write your test here
+describe('throwCustomError', (): void => {
+  test('should throw custom error', (): void => {
+    expect(() => throwCustomError()).toThrowError(MyAwesomeError);
+  });
+});
+describe('rejectCustomError', (): void => {
+  test('should reject custom error', async (): Promise<void> => {
+    await expect(rejectCustomError()).rejects.toThrowError(MyAwesomeError);
   });
 });
