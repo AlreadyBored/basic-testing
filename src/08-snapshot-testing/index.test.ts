196c5af4805a0ea05870d0a7415f47d0cc17edc7
PavelOreshkin <poreshkin@samokat.ru>
2024-10-19 20:42:50 +0400

    feat: solve 08-snapshot-testing task

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,27 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const elements = [1, 1, 1];
+    const expectedLinkedList = {
+      value: 1,
+      next: {
+        value: 1,
+        next: {
+          value: 1,
+          next: { value: null, next: null },
+        },
+      },
+    };
+
+    expect(generateLinkedList(elements)).toStrictEqual(expectedLinkedList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const elements = [2, 2, 2];
+
+    expect(generateLinkedList(elements)).toMatchSnapshot();
   });
 });
