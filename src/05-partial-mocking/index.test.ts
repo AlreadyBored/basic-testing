44dc756b54853e67ff559f41d86825e84dd9679e
samvel <s.saakyana@trainingspace.ru>
2024-10-20 04:15:13 +0400

    feat: 05-partial-mocking

--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -1,8 +1,16 @@
 // Uncomment the code below and write your tests
-// import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
+import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
 
 jest.mock('./index', () => {
-  // const originalModule = jest.requireActual<typeof import('./index')>('./index');
+  const originalModule =
+    jest.requireActual<typeof import('./index')>('./index');
+
+  return {
+    ...originalModule,
+    mockOne: jest.fn(),
+    mockTwo: jest.fn(),
+    mockThree: jest.fn(),
+  };
 });
 
 describe('partial mocking', () => {
@@ -11,10 +19,20 @@ describe('partial mocking', () => {
   });
 
   test('mockOne, mockTwo, mockThree should not log into console', () => {
-    // Write your test here
+    console.log = jest.fn();
+    mockOne();
+    mockTwo();
+    mockThree();
+
+    expect(console.log).not.toHaveBeenCalled();
   });
 
   test('unmockedFunction should log into console', () => {
-    // Write your test here
+    console.log = jest.fn();
+
+    unmockedFunction();
+
+    expect(console.log).toHaveBeenCalled();
+    expect(console.log).toHaveBeenCalledWith('I am not mocked');
   });
 });
