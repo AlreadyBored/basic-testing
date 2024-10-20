a7f83d86cb503ba94fc28c1d7d389f558a60bd61
samvel <s.saakyana@trainingspace.ru>
2024-10-20 13:45:17 +0400

    feat: 08-snapshot-testing tests

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,33 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const input = [1, 2, 3];
+    const expectedOutput = {
+      value: 1,
+      next: {
+        value: 2,
+        next: {
+          value: 3,
+          next: { value: null, next: null },
+        },
+      },
+    };
+
+    const result = generateLinkedList(input);
+
+    expect(JSON.stringify(result)).toEqual(JSON.stringify(expectedOutput));
+
+    expect(result).toStrictEqual(expectedOutput);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const input = [1, 2, 3, 4, 5];
+    const result = generateLinkedList(input);
+
+    expect(result).toMatchSnapshot();
   });
 });
