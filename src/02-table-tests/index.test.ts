1eb78a9466d52be547e43735b894b9f9a09f400d
ivan.sigaev <ivan.sigaev@check24.de>
2024-10-21 20:49:09 +0200

    feat: add table tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,41 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 0, action: Action.Add, expected: 2 },
+  { a: 3, b: -2, action: Action.Add, expected: 1 },
+  { a: 1, b: 2, action: Action.Subtract, expected: -1 },
+  { a: 2, b: -2, action: Action.Subtract, expected: 4 },
+  { a: 3, b: 0, action: Action.Subtract, expected: 3 },
+  { a: 1, b: 2, action: Action.Divide, expected: 0.5 },
+  { a: 2, b: -2, action: Action.Divide, expected: -1 },
+  { a: 3, b: 0, action: Action.Divide, expected: Infinity },
+  { a: 1, b: 2, action: Action.Multiply, expected: 2 },
+  { a: 2, b: -2, action: Action.Multiply, expected: -4 },
+  { a: 3, b: 0, action: Action.Multiply, expected: 0 },
+  { a: 1, b: 2, action: Action.Exponentiate, expected: 1 },
+  { a: 2, b: -2, action: Action.Exponentiate, expected: 0.25 },
+  { a: 3, b: 0, action: Action.Exponentiate, expected: 1 },
+];
+
+const nullTestCases = [
+  { a: 1, b: '2', action: Action.Add },
+  { a: 1, b: null, action: Action.Add },
+  { a: 2, b: 0, action: '!' },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
-  });
-  // Consider to use Jest table tests API to test all cases above
+  test.each(testCases)(
+    'should return $expected when $a is $action $b',
+    ({ a, b, action, expected }) => {
+      expect(simpleCalculator({ a, b, action })).toBe(expected);
+    },
+  );
+
+  test.each(nullTestCases)(
+    'should return null when $a is $action $b',
+    ({ a, b, action }) => {
+      expect(simpleCalculator({ a, b, action })).toBeNull();
+    },
+  );
 });
