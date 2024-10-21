17fa660d0c2ebc505195d9f11cddbd974ed1274c
dostonlv <dostonlv@icloud.com>
2024-10-21 12:44:43 +0500

    module #7 tests done

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,35 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index'; 
 
 describe('throttledGetDataFromApi', () => {
+  const actualBaseURL = "https://jsonplaceholder.typicode.com";
+  const path = "/posts/1";
   test('should create instance with provided base url', async () => {
-    // Write your test here
-  });
+    const createInstanceSpy = jest.spyOn(axios, 'create');
+    await throttledGetDataFromApi(path);
+    expect(createInstanceSpy).toBeCalledWith({ baseURL: actualBaseURL });  });
 
-  test('should perform request to correct provided url', async () => {
-    // Write your test here
-  });
+    test('should perform request to correct provided url', async () => {
+      const createInstanceSpy = jest.spyOn(axios, 'create');
+      createInstanceSpy.mockImplementationOnce((config) => {
+        const result = axios.create(config);
+        const getSpy = jest.spyOn(result, 'get');
+        expect(getSpy).toBeCalledWith(path);
+        return result;
+      })
+      await throttledGetDataFromApi(path);
+    });
 
-  test('should return response data', async () => {
-    // Write your test here
+
+ test('should return response data', async () => {
+    const result = await throttledGetDataFromApi(path);
+    const actualData = {
+      "userId": 1,
+      "id": 1,
+      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    };
+    expect(result).toStrictEqual(actualData);
   });
 });
