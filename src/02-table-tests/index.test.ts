4a307bb4a72bc5b3503816403cc2a08f76754a57
Alesia Branavitskaya <alesun4ik@gmail.com>
2024-10-21 22:12:11 +0300

    feat: add 2 second test

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,16 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import {  simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+    { a: 5, b: 5, action: Action.Add, expected: 10 },
+    { a: 15, b: 10, action: Action.Subtract, expected: 5 },
+    { a: 15, b: 10, action: Action.Multiply, expected: 150 },
+    { a: 10, b: 2, action: Action.Divide, expected: 5 },
+    { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
+];
 
-describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
-  });
-  // Consider to use Jest table tests API to test all cases above
+test.each(testCases)('should return %s when input is %i', ({a, b, action, expected}) => {
+  expect(simpleCalculator({a, b, action})).toBe(expected);
 });
+
+
