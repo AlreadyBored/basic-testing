1c5e69c804ce61e52bb9f379e66c8f4bbcdd6d07
dostonlv <dostonlv@icloud.com>
2024-10-21 12:48:06 +0500

    module #8 tests done

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,31 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const actualList = {
+      value: 1,
+      next: {
+        value: 2,
+        next: {
+          value: 3,
+          next: {
+            value: null,
+            next: null
+          }
+        }
+      }
+    }
+    const values1 = [1,2,3];
+    const list = generateLinkedList(values1);
+    expect(list).toStrictEqual(actualList)
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
     // Write your test here
+    const values2 = [0,1,1,2,3,5,8,13,21];
+    const list = generateLinkedList(values2);
+    expect(list).toMatchSnapshot();
   });
 });
