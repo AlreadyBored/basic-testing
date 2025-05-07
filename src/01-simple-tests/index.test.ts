d2bab0cd95bb1c9935a8bc691b1ab2ba8483805b
meta-panic <ritaritarita2013@gmail.com>
2025-05-08 03:04:02 +0400

    feat: implement unit tests for simpleCalculator functionality

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,70 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const sum = simpleCalculator({ a: 2, b: 2, action: Action.Add });
+
+    expect(sum).toBe(4);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const subtracttion = simpleCalculator({
+      a: 10,
+      b: 2,
+      action: Action.Subtract,
+    });
+
+    expect(subtracttion).toBe(8);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const multiplycation = simpleCalculator({
+      a: 10,
+      b: 2,
+      action: Action.Multiply,
+    });
+
+    expect(multiplycation).toBe(20);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const division = simpleCalculator({
+      a: 10,
+      b: 2,
+      action: Action.Divide,
+    });
+
+    expect(division).toBe(5);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const exponention = simpleCalculator({
+      a: 2,
+      b: 4,
+      action: Action.Exponentiate,
+    });
+
+    expect(exponention).toBe(16);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const nonExistendAction = simpleCalculator({
+      a: 2,
+      b: 4,
+      action: 'non existend action',
+    });
+
+    expect(nonExistendAction).toBe(null);
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const nonExistendAction = simpleCalculator({
+      a: '2',
+      b: '4',
+      action: Action.Exponentiate,
+    });
+
+    expect(nonExistendAction).toBe(null);
   });
 });
