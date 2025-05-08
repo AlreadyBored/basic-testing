22ba4f48eca61f3108c3c5c1a427690ade8b78ce
iurii-oleinik <iurii.oleinik@alpeso.io>
2025-05-08 19:46:21 +0300

    feat: add snapshots tests

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,20 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    // Check match by expect(...).toStrictEqual(...)
+    expect(generateLinkedList([1, 2])).toStrictEqual({
+      value: 1,
+      next: { value: 2, next: { value: null, next: null } },
+    });
+
+    expect(generateLinkedList([])).toStrictEqual({ value: null, next: null });
   });
 
-  // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    // Check match by comparison with snapshot
+    expect(generateLinkedList(['a', 'b', 'c'])).toMatchSnapshot();
+    expect(generateLinkedList([])).toMatchSnapshot();
   });
 });
