7a515f6e848222ebdf171ad94270185e0be45d38
iurii-oleinik <iurii.oleinik@alpeso.io>
2025-05-08 19:45:04 +0300

    feat: add mocl lib tests

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,43 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
+jest.mock('lodash', () => ({
+  throttle: jest.fn((fn) => fn), // Mock throttle to directly call the function
+}));
+
+const PATH = '/test';
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
 describe('throttledGetDataFromApi', () => {
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const mockAxiosInstance = { get: jest.fn().mockResolvedValue({ data: 'fakeData' }) };
+    (axios.create as jest.Mock).mockReturnValue(mockAxiosInstance);
+
+    await throttledGetDataFromApi(PATH);
+
+    expect(axios.create).toHaveBeenCalledWith({ baseURL: BASE_URL });
+    expect(mockAxiosInstance.get).toHaveBeenCalledWith(PATH);
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const mockAxiosInstance = { get: jest.fn().mockResolvedValue({ data: 'fakeData' }) };
+    (axios.create as jest.Mock).mockReturnValue(mockAxiosInstance);
+
+    await throttledGetDataFromApi('/users/1');
+    expect(mockAxiosInstance.get).toHaveBeenCalledWith('/users/1');
+
+    await throttledGetDataFromApi('/users/2');
+    expect(mockAxiosInstance.get).toHaveBeenCalledWith('/users/2');
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const mockResponseData1 = { data: 'fakeData' };
+    const mockAxiosInstance = { get: jest.fn().mockResolvedValue({ data: mockResponseData1 }) };
+    (axios.create as jest.Mock).mockReturnValue(mockAxiosInstance);
+
+    const result = await throttledGetDataFromApi('/users');
+    expect(result).toEqual(mockResponseData1);
   });
 });
