bfc8dac0bca6f88c24eeaff5e9fe4198fb328a5c
meta-panic <ritaritarita2013@gmail.com>
2025-05-09 00:53:05 +0400

    feat: finish 7th task

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,57 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios, { AxiosInstance } from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.useFakeTimers();
 
 describe('throttledGetDataFromApi', () => {
+  const baseURL = 'https://jsonplaceholder.typicode.com';
+  const relativePath = 'path/1';
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const mockAxiosInstance = {
+      get: jest.fn().mockResolvedValue({ data: 'mock data' }),
+    };
+
+    const spyAxiosCreate = jest
+      .spyOn(axios, 'create')
+      .mockReturnValue(mockAxiosInstance as unknown as AxiosInstance);
+    throttledGetDataFromApi(relativePath);
+
+    expect(spyAxiosCreate).toHaveBeenLastCalledWith({ baseURL: baseURL });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const responseMock = { data: 'Test Data' };
+    const getMock = {
+      get: jest.fn().mockResolvedValue(responseMock),
+    };
+    const createMock = jest.fn().mockReturnValue(getMock);
+    axios.create = createMock;
+    throttledGetDataFromApi(relativePath);
+    jest.runAllTimers();
+    expect(axios.create).toHaveBeenCalledWith({
+      baseURL,
+    });
+    expect(getMock.get).toHaveBeenCalledWith(relativePath);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const responseMock = { data: 'Test Data' };
+    const getMock = {
+      get: jest.fn().mockResolvedValue(responseMock),
+    };
+    const createMock = jest.fn().mockReturnValue(getMock);
+    axios.create = createMock;
+    throttledGetDataFromApi(relativePath);
+    jest.runAllTimers();
+    const result = await throttledGetDataFromApi(relativePath);
+    expect(result).toEqual(responseMock.data);
   });
+});
+
+afterAll(() => {
+  jest.useRealTimers();
 });
