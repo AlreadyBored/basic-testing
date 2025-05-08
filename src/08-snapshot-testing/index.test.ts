5709dd6e7815d7c91360d4a155264116f7b4e8c1
meta-panic <ritaritarita2013@gmail.com>
2025-05-09 01:06:47 +0400

    feat: add unit tests for generateLinkedList with snapshot verification

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,29 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
+const mock = {
+  next: {
+    next: {
+      next: {
+        next: null,
+        value: null,
+      },
+      value: 3,
+    },
+    value: 2,
+  },
+  value: 1,
+};
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const generatedArray = generateLinkedList([1, 2, 3]);
+
+    expect(generatedArray).toStrictEqual(mock);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const generatedArray = generateLinkedList([2, 3]);
+
+    expect(generatedArray).toMatchSnapshot();
   });
 });
