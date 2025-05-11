76e78a453e9a7058d4ea1c7e8ce9f2875b58fbd6
aleksawsea <alexandrapynzar57@gmail.com>
2025-05-12 01:26:52 +0400

    feat: implement 07-mocking-lib-api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,60 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+jest.useFakeTimers();
+jest.mock('axios');
+
+import axios from 'axios';
+import { throttledGetDataFromApi, THROTTLE_TIME } from './index';
+
+jest.mock('lodash', () => {
+  const original = jest.requireActual('lodash');
+  return {
+    ...original,
+    throttle: jest.fn(
+      <T extends (...args: unknown[]) => unknown>(fn: T, wait: number): T => {
+        void wait;
+        return fn;
+      },
+    ),
+  };
+});
 
 describe('throttledGetDataFromApi', () => {
+  let mockGet: jest.Mock;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mockGet = jest.fn().mockResolvedValue({ data: { test: 'test1' } });
+    (axios.create as jest.Mock).mockReturnValue({ get: mockGet });
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const url = '/test';
+    const promise = throttledGetDataFromApi(url);
+    jest.advanceTimersByTime(THROTTLE_TIME);
+    await promise;
+
+    expect(axios.create).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const url = '/test';
+    const promise = throttledGetDataFromApi(url);
+    jest.advanceTimersByTime(THROTTLE_TIME);
+    await promise;
+    expect(mockGet).toHaveBeenCalledWith(url);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const url = '/test';
+    const promise = await throttledGetDataFromApi(url);
+    jest.advanceTimersByTime(THROTTLE_TIME);
+    const result = await promise;
+    expect(result).toEqual({ test: 'test1' });
   });
 });
