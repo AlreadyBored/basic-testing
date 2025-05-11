44918ec294e3bdd454685956f01ffb11ff18c659
dsalangin <dsalangin74@gmail.com>
2025-05-11 23:26:48 +0300

    feat: add tests for snapshot testing block

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,23 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList([1, 1])).toStrictEqual({
+      value: 1,
+      next: {
+        value: 1,
+        next: {
+          value: null,
+          next: null,
+        },
+      },
+    });
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList([2, 2])).toMatchSnapshot();
   });
 });
