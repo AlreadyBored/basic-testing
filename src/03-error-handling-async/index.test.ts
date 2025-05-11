552500c0a0f0fff9a95f0fd31e6728f39269102c
kravchenski <max5045955@gmail.com>
2025-05-11 11:45:31 +0200

    feat: complete errors handling task

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -1,30 +1,39 @@
 // Uncomment the code below and write your tests
-// import { throwError, throwCustomError, resolveValue, MyAwesomeError, rejectCustomError } from './index';
+import {
+  throwError,
+  throwCustomError,
+  resolveValue,
+  rejectCustomError,
+  MyAwesomeError,
+} from "./index";
 
-describe('resolveValue', () => {
-  test('should resolve provided value', async () => {
-    // Write your test here
+describe("resolveValue", () => {
+  test("should resolve provided value", async () => {
+    const resolveValueMsg = "Hello";
+    const result = await resolveValue(resolveValueMsg);
+    expect(result).toBe(resolveValueMsg);
   });
 });
 
-describe('throwError', () => {
-  test('should throw error with provided message', () => {
-    // Write your test here
+describe("throwError", () => {
+  test("should throw error with provided message", () => {
+    const resolveValueMsg = "Hello";
+    expect(() => throwError(resolveValueMsg)).toThrow(resolveValueMsg);
   });
 
-  test('should throw error with default message if message is not provided', () => {
-    // Write your test here
-  });
-});
-
-describe('throwCustomError', () => {
-  test('should throw custom error', () => {
-    // Write your test here
+  test("should throw error with default message if message is not provided", () => {
+    expect(() => throwError()).toThrow("Oops!");
   });
 });
 
-describe('rejectCustomError', () => {
-  test('should reject custom error', async () => {
-    // Write your test here
+describe("throwCustomError", () => {
+  test("should throw custom error", () => {
+    expect(() => throwCustomError()).toThrow(MyAwesomeError);
+  });
+});
+
+describe("rejectCustomError", () => {
+  test("should reject custom error", async () => {
+    expect(rejectCustomError()).rejects.toThrow(MyAwesomeError);
   });
 });
