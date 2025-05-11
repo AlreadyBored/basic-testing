e25eb1fffeea287a533e721252d305eabf24b2d1
N1kolozz <mr.osievi5@gmail.com>
2025-05-11 23:23:16 +0400

    test: implement tests for generateLinkedList function and add snapshot

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,29 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const input = [1, 2, 3];
+    const expected = {
+      value: 1,
+      next: {
+        value: 2,
+        next: {
+          value: 3,
+          next: {
+            value: null,
+            next: null,
+          },
+        },
+      },
+    };
+
+    expect(generateLinkedList(input)).toStrictEqual(expected);
   });
 
-  // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const input = ['a', 'b', 'c'];
+    const result = generateLinkedList(input);
+    expect(result).toMatchSnapshot();
   });
 });
