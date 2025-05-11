f61d616c138dcd36f644da8c2ec21b2a9925d0a3
Irene Strigo <irene.strigo@gmail.com>
2025-05-11 19:28:45 +0200

    feat: add simple tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,71 @@
-// Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
+type RawCalculatorInput = {
+  a: unknown;
+  b: unknown;
+  action: unknown;
+};
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const rawInput: RawCalculatorInput = {
+      a: 1,
+      b: 2,
+      action: Action.Add,
+    };
+    expect(simpleCalculator(rawInput)).toEqual(3);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const rawInput: RawCalculatorInput = {
+      a: 3,
+      b: 2,
+      action: Action.Subtract,
+    };
+    expect(simpleCalculator(rawInput)).toEqual(1);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const rawInput: RawCalculatorInput = {
+      a: 2,
+      b: 3,
+      action: Action.Multiply,
+    };
+    expect(simpleCalculator(rawInput)).toEqual(6);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const rawInput: RawCalculatorInput = {
+      a: 10,
+      b: 5,
+      action: Action.Divide,
+    };
+    expect(simpleCalculator(rawInput)).toEqual(2);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const rawInput: RawCalculatorInput = {
+      a: 2,
+      b: 3,
+      action: Action.Exponentiate,
+    };
+    expect(simpleCalculator(rawInput)).toEqual(8);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const rawInput: RawCalculatorInput = {
+      a: 2,
+      b: 3,
+      action: !Action,
+    };
+    expect(simpleCalculator(rawInput)).toEqual(null);
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const rawInput: RawCalculatorInput = {
+      a: undefined,
+      b: null,
+      action: Action,
+    };
+    expect(simpleCalculator(rawInput)).toEqual(null);
   });
 });
