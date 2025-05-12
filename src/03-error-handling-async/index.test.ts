ca9cf0c62a9d339caacf61134e3841e395d18ba8
Sviatoslav Pokhvalenko <pokhvalenkosvyat@gmail.com>
2025-05-12 11:20:53 +0200

    Solved problem in error handling tests

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -20,7 +20,7 @@ describe('throwError', () => {
   });
 
   test('should throw error with default message if message is not provided', () => {
-    expect(() => throwError()).toThrow('No error message provided.');
+    expect(() => throwError()).toThrow('Oops!');
   });
 });
 
